ddc4a8cf56b550f32602e7b9c8fe924213740bde
nicolegugu93 <nicole.guevaragutierrez@gmail.com>
2025-11-12 12:06:56 +0100

    new change at giftpage (#71)

--- src/pages/GiftPage.jsx
+++ src/pages/GiftPage.jsx
@@ -49,7 +49,7 @@ const GiftPage = () => {
   
   const handleButtonClick = () => {
     navigate("/app/subscription/checkout");
-    window.scrollTo(0, 0); // 👈 Asegura que ambas redirecciones empiecen desde arriba
+    window.scrollTo(0, 0); 
   };
 
   return (
